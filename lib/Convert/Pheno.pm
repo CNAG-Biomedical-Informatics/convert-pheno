7eba71985bddfa5a2a7a1d7db6e9ce4db40f67f2
Manuel Rueda <rueda.manu@gmail.com>
2022-06-15 12:41:06 +0200

    Source commit

---
+++ lib/Convert/Pheno.pm
@@ -0,0 +1,134 @@
+package Convert::Pheno;
+
+use strict;
+use warnings;
+use autodie;
+use JSON::XS;
+use Path::Tiny;
+
+use constant DEVEL_MODE   => 0;
+use vars qw{
+  $VERSION
+  @ISA
+  @EXPORT
+};
+ 
+@ISA    = qw( Exporter );
+@EXPORT = qw( &write_json );
+ 
+sub new {
+
+    my ( $class, $self ) = @_;
+    bless $self, $class;
+    return $self;
+}
+
+sub map_pxf2bff {
+
+    my $self = shift;
+    my $data = read_json( $self->{pxf_file} );
+
+    # Get cursors for 1D terms
+    my $interpretation = $data->{interpretation};
+    my $phenopacket    = $data->{phenopacket};
+
+    ####################################
+    # START MAPPING TO BEACON V2 TERMS #
+    ####################################
+
+    # NB1: In general, we'll only load terms that exist
+    # NB2: In PXF some terms are = []
+    my $individual;
+
+    # ========
+    # diseases
+    # ========
+    $individual->{diseases} =
+      [ map { $_ = { "diseaseCode" => $_->{term} } }
+          @{ $phenopacket->{diseases} } ]
+      if exists $phenopacket->{diseases};
+
+    # ==
+    # id
+    # ==
+    $individual->{id} = $phenopacket->{subject}{id}
+      if exists $phenopacket->{subject}{id};
+
+    # ====
+    # info
+    # ====
+
+    # **** $data->{phenopacket} ****
+    $individual->{info}{phenopacket}{dateOfBirth} =
+      $phenopacket->{subject}{dateOfBirth};
+    for my $term (qw (dateOfBirth genes meta_data variants)) {
+        $individual->{info}{phenopacket}{$term} = $phenopacket->{$term}
+          if exists $phenopacket->{$term};
+    }
+
+    # **** $data->{interpretation} ****
+    $individual->{info}{interpretation}{phenopacket}{meta_data} =
+      $interpretation->{phenopacket}{meta_data};
+
+    # <diseases> and <phenotypicFeatures> are identical to those of $data->{phenopacket}{diseases,phenotypicFeatures}
+    for my $term (
+        qw (diagnosis diseases resolutionStatus phenotypicFeatures genes variants)
+      )
+    {
+        $individual->{info}{interpretation}{$term} = $interpretation->{$term}
+          if exists $interpretation->{$term};
+    }
+
+    # ==================
+    # phenotypicFeatures
+    # ==================
+    $individual->{phenotypicFeatures} = [
+        map {
+            $_ = {
+                "excluded" => (
+                    exists $_->{negated} ? JSON::XS::true : JSON::XS::false
+                ),
+                "featureType" => $_->{type}
+            }
+        } @{ $phenopacket->{phenotypicFeatures} }
+      ]
+      if exists $phenopacket->{phenotypicFeatures};
+
+    # ===
+    # sex
+    # ===
+    $individual->{sex} = map_sex( $phenopacket->{subject}{sex} )
+      if exists $phenopacket->{subject}{sex};
+
+    ##################################
+    # END MAPPING TO BEACON V2 TERMS #
+    ##################################
+
+    # print Dumper $individual;
+    return $individual;
+}
+
+sub map_sex {
+
+    my $str = lc(shift);
+    my %sex = ( male => 'NCIT:C20197', female => 'NCIT:C16576' );
+    return { "id" => "$sex{$str}", "label" => "$str" };
+}
+
+sub read_json {
+
+    my $json_file = shift;
+    my $str       = path($json_file)->slurp_utf8;
+    my $json      = decode_json($str);              # Decode to Perl data structure
+    return $json;
+}
+
+sub write_json {
+
+    my ( $file, $json_array ) = @_;
+    my $json = JSON::XS->new->utf8->canonical->pretty->encode($json_array);
+    path($file)->spew_utf8($json);
+    return 1;
+}
+
+1;
