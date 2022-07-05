b569957b4f8bfd11ef2b6811dd13701ed0eb3c7e
Manuel Rueda <rueda.manu@gmail.com>
2022-07-05 11:44:47 +0200

    Source commit

--- lib/Convert/Pheno.pm
+++ lib/Convert/Pheno.pm
@@ -122,6 +122,9 @@ sub redcap2bff {
     my $self    = shift;
     my $in_file = $self->{in_file};
 
+    # Load ReadCap dictionary
+    my $rcd = load_redcap_dictionary( $self->{'redcap_dictionary'} );
+
     # Define split record separator
     my @exts = qw(.csv .tsv .txt);
     my ( undef, undef, $ext ) = fileparse( $in_file, @exts );
@@ -202,8 +205,7 @@ sub redcap2bff {
         # ===
         # sex
         # ===
-        #print Dumper $data;
-        $individual->{sex} = map_sex( $element->{sex} )
+        $individual->{sex} = map_sex( $rcd->{sex}{_labels}{ $element->{sex} } )
           if ( exists $element->{sex} && $element->{sex} );
 
         ##################################
@@ -218,14 +220,12 @@ sub redcap2bff {
 
 sub map_sex {
 
-    my $str = lc(shift);
+    my $str = shift;
     my %sex = (
         male   => 'NCIT:C20197',
-        female => 'NCIT:C16576',
-        '2'    => 'NCIT:C20197',
-        '1'    => 'NCIT:C16576'
+        female => 'NCIT:C16576'
     );
-    return { "id" => "$sex{$str}", "label" => "$str" };
+    return { id => $sex{ lc($str) }, label => $str };
 }
 
 sub read_json {
@@ -242,6 +242,73 @@ sub write_json {
     my $json = JSON::XS->new->utf8->canonical->pretty->encode($json_array);
     path($file)->spew_utf8($json);
     return 1;
+}
+
+sub load_redcap_dictionary {
+
+    my $in_file = shift;
+
+    # Define split record separator
+    my @exts = qw(.csv .tsv .txt);
+    my ( undef, undef, $ext ) = fileparse( $in_file, @exts );
+
+    #########################################
+    #     START READING CSV|TSV|TXT FILE    #
+    #########################################
+    open my $fh_in, '<:encoding(utf8)', $in_file;
+
+    # We'll read the header to assess separators in <txt> files
+    chomp( my $tmp_header = <$fh_in> );
+
+    # Defining separator
+    my $separator =
+        $ext eq '.csv' ? ';'
+      : $ext eq '.tsv' ? "\t"
+      :                  ' ';
+
+    # Defining variables
+    my $data = {};                  #AoH
+    my $csv  = Text::CSV_XS->new(
+        {
+            binary    => 1,
+            auto_diag => 1,
+            sep_char  => $separator
+        }
+    );
+
+    # Loading header fields into $header
+    $csv->parse($tmp_header);
+    my $header = [ $csv->fields() ];
+
+    # Now proceed with the rest of the file
+    while ( my $row = $csv->getline($fh_in) ) {
+
+        # We store the data as an AoH $data
+        my $tmp_hash;
+
+        for my $i ( 0 .. $#{$header} ) {
+
+            # We keep key>/value as they are
+            $tmp_hash->{ $header->[$i] } = $row->[$i];
+
+            # For the key having lavels, we create a new ad hoc key '_labels'
+            # 'Choices, Calculations, OR Slider Labels' => '1, Female|2, Male|3, Other|4, not available',
+            if ( $header->[$i] eq 'Choices, Calculations, OR Slider Labels' ) {
+                my @tmp =
+                  map { s/^\s//; s/\s+$//; $_; } ( split /\||,/, $row->[$i] );
+                $tmp_hash->{_labels} = {@tmp} if @tmp % 2 == 0;
+            }
+        }
+
+        # Now we create the 1D of the hash with 'Variable / Field Name'
+        my $key = $tmp_hash->{'Variable / Field Name'};
+
+        # And we nest the hash inside
+        $data->{$key} = $tmp_hash;
+    }
+
+    close $fh_in;
+    return $data;
 }
 
 1;
