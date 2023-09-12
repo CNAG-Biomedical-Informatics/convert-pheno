d153f08e9f113412e05768d90ac7825c75ac800e
Manuel Rueda <rueda.manu@gmail.com>
2023-09-12 11:10:53 +0200

    Submit 0.12_4 to CPAN

--- lib/Convert/Pheno.pm
+++ lib/Convert/Pheno.pm
@@ -36,7 +36,7 @@ our @EXPORT =
 use constant DEVEL_MODE => 0;
 
 # Global variables:
-our $VERSION   = '0.12_3';
+our $VERSION   = '0.12_4';
 our $share_dir = dist_dir('Convert-Pheno');
 
 ############################################
