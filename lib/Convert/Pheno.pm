0d068a292269a153cec986752f7b74fce523a565
Manuel Rueda <rueda.manu@gmail.com>
2023-12-14 06:06:58 +0100

    Add dev to version

--- lib/Convert/Pheno.pm
+++ lib/Convert/Pheno.pm
@@ -40,7 +40,7 @@ $SIG{__WARN__} = sub { warn "Warn: ", @_ };
 $SIG{__DIE__}  = sub { die "Error: ", @_ };
 
 # Global variables:
-our $VERSION   = '0.15-dev';
+our $VERSION   = '0.15_1';
 our $share_dir = dist_dir('Convert-Pheno');
 
 ############################################
